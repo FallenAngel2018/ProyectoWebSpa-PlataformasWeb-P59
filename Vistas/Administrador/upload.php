efc583760cede2fff144e2f14f3847daeddf1f14
Joshua Martinez <jmartinezb3@est.ups.edu.ec>
2021-12-29 00:38:37 -0500

    Subida de imagenes
    Se crea una carpeta de Test con codigo de prueba para implementar la subida de imagenes corriendo en el servidor de PHP

--- Vistas/Administrador/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-// $targetPath = "../../Recursos/ImagenesServicios" . basename($_FILES["inpFile"]["name"]);
-$targetPath = "../../Recursos/ImagenesServicios/" . basename($_FILES["fotografia"]["name"]);
-
-// move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
-move_uploaded_file($_FILES["fotografia"]["tmp_name"], $targetPath);
-
