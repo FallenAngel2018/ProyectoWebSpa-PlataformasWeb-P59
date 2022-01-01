e726dbf2eeeef6cf7f093e3fa0f9f53977338106
FallenAngel <isaacobesso2012@hotmail.com>
2022-01-01 15:55:41 -0500

    Admin - showId
    Método para iterar sobre la lista de servicios, y setear los campos del formulario de Servicio en base al id que se le pase como argumento.

--- Componentes/Servicio/Modelo.js
+++ Componentes/Servicio/Modelo.js
@@ -20,19 +20,8 @@ export default class Servicio {
         this.estado = estado // ACTIVO o INACTIVO
     }
 
-    // services = new Service() = [
-    //     {"nombre": "name1", "descripcion": "descripcion", "duracion": "5 horas", "fotografia": "url/1.jpg", 
-    //     "precio": 20.4,"tipo_servicio": "VIP"},
 
-    //     {"nombre": "name2", "descripcion": "descripcion2", "duracion": "8 horas", "fotografia": "url/2.jpg", 
-    //     "precio": 5,"tipo_servicio": "Normal"},
-        
-    //     {"nombre": "name3", "descripcion3": "descripcion3", "duracion3": "1 hora", "fotografia": "url/3.jpg", 
-    //     "precio": 15.85,"tipo_servicio": "VIP"}
-        
-    // ];
- 
-
+    // Definir función
     static saveService(service) {
         // var servicio = Object.assign(this.Servicio(), data);
 
@@ -52,14 +41,6 @@ export default class Servicio {
     }
 
     // Definir función
-    saludar(fn) {
-        console.log( `Hola soy ${this.nombre} ${this.apellido}` )
-        if (fn) {
-            fn(this.nombre, this.apellido)
-        }
-    }
-
-    // Definir función
     soy_alto(fn) {
         if (this.altura >= 1.7) {
             console.log( `Mi altura es ${this.altura} y soy alto.` )
@@ -71,6 +52,7 @@ export default class Servicio {
             fn( this.nombre )
         }
     }
+
 }
 
 /*
@@ -85,15 +67,15 @@ export default class Servicio {
 
 // Lista estática de servicios
 export const lista_servicios = new Array();
-                                                // 5 horas en minutos
-lista_servicios.push(new Servicio(1, "Masaje asiático", "descripcion", 300, "url/1.jpg", 20.4, "VIP", true));
-                                                // 8 horas en minutos
-lista_servicios.push(new Servicio(2, "Acupuntura abdominal", "descripcion2", 480, "url/2.jpg", 50, "Normal", false));
-lista_servicios.push(new Servicio(3, "Depilación", "descripcion3", 30, "url/2.jpg", 12.35, "Normal", true));
-lista_servicios.push(new Servicio(4, "Facial", "descripcion4", 30, "url/2.jpg", 15.50, "Normal", true));
-lista_servicios.push(new Servicio(5, "Masaje", "descripcion5", 60, "url/2.jpg", 20, "Normal", true));
+                                                        // 5 horas en minutos                2 = VIP
+lista_servicios.push(new Servicio(1, "Masaje asiático", "descripcion", 300, "url/1.jpg", 20.4, "2", true));
+                                                               // 8 horas en minutos u horas     1 = Normal
+lista_servicios.push(new Servicio(2, "Acupuntura abdominal", "descripcion2", 480, "url/2.jpg", 50, "1", false));
+lista_servicios.push(new Servicio(3, "Depilación", "descripcion3", 30, "url/2.jpg", 12.35, "1", true));
+lista_servicios.push(new Servicio(4, "Facial", "descripcion4", 30, "url/2.jpg", 15.50, "1", true));
+lista_servicios.push(new Servicio(5, "Masaje", "descripcion5", 60, "url/2.jpg", 20, "1", true));
 lista_servicios.push(new Servicio(6, "Masaje VIP cuerpo completo", "Masaje de cuerpo completo con aloe vera, rocas calientes y sí se desea en el momento también con acupuntura especializada."
-, 90, "url/2.jpg", 40.35, "VIP", true));
+, 90, "url/2.jpg", 40.35, "2", true));
 
 
 
